47d18999f98852c624b56974e25f5a534f5725e3
500WAN\hujs <assqmyyyrhcjcx@aliyun.com>
2018-06-22 21:08:08 +0800

    add article

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from SSO.models import User
 
 # Create your models here.
+class  Article(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=20)
+    content = models.TextField(blank=True)
+    createtime = models.DateField()
+    updatetime = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+
+    def __unicode__(self):
+        return u'%s' % self.name
